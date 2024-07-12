f0236d57ff72e3db60bea3651fd629b6df94050c
Maciej Kusztal <84096120+mkusztal@users.noreply.github.com>
2024-07-12 22:47:02 +0300

    feat: add redux part of one product fetching (#11)

--- client/src/redux/yerbamateReducer.ts
+++ client/src/redux/yerbamateReducer.ts
@@ -2,11 +2,12 @@ import { initialState } from "./initialState";
 import { API_URL } from "../config/urls";
 import { IYerbamate } from "../interfaces/IYerbamate";
 import { Dispatch } from "redux";
-
 export const getYerbamate = ({ yerbamate }: any): IYerbamate[] => yerbamate;
+export const getSingleYerbamate = ({ yerbamate }: any): IYerbamate => yerbamate;
 
 const createActionName = (actionName: string) => `app/yerbamate/${actionName}`;
-const GET_YERBAMATE = createActionName("GET_YERBAMATE");
+const GET_ALL_YERBAMATE = createActionName("GET_ALL_YERBAMATE");
+const GET_ONE_YERBAMATE = createActionName("GET_ONE_YERBAMATE");
 
 export const fetchYerbamate = (): any => {
   return (dispatch: Dispatch) => {
@@ -14,7 +15,7 @@ export const fetchYerbamate = (): any => {
       .then((res) => res.json())
       .then((data) => {
         dispatch({
-          type: GET_YERBAMATE,
+          type: GET_ALL_YERBAMATE,
           payload: data,
         });
       })
@@ -24,10 +25,35 @@ export const fetchYerbamate = (): any => {
   };
 };
 
+export const fetchOneYerbamate = (id: string) => {
+  if (typeof id !== "string" || id === "") {
+    console.log("Invalid id");
+    return;
+  }
+  return (dispatch: Dispatch) => {
+    fetch(API_URL + `api/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({
+          type: GET_ONE_YERBAMATE,
+          payload: data,
+        });
+      })
+      .catch((err) => {
+        console.log("Error fetching single product: ", err);
+      });
+  };
+};
+
 export const yerbamateReducer = (state = initialState, action: any) => {
   switch (action.type) {
-    case GET_YERBAMATE:
+    case GET_ALL_YERBAMATE:
       return action.payload;
+    case GET_ONE_YERBAMATE:
+      return [
+        ...state.filter((item: IYerbamate) => item.id !== action.payload.id),
+        action.payload,
+      ];
     default:
       return state;
   }
