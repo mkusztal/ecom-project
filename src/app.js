a81c9004d4105398b02017af5a16e7a4471c8df1
mKusztal <maciej.kusztal1995@gmail.com>
2024-10-24 04:05:52 +0300

    fix: database and api connections

--- src/app.js
+++ src/app.js
@@ -12,9 +12,12 @@ const registration = require("./routes/users.routes");
 const email = require("./routes/email.routes");
 const { errorHandler, notFoundHandler } = require("./utils/errorHandlers");
 dotenv.config({ path: "./.env" });
-const MySQLStore = require("express-mysql-session")(session);
+// const MySQLStore = require("express-mysql-session")(session);
+const pgSession = require("connect-pg-simple")(session);
+
 const app = express();
 const port = process.env.PORT;
+const db_port = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432;
 
 const generateCSRFTToken = () => {
   return crypto.randomBytes(32).toString("hex");
@@ -34,12 +37,26 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(helmet());
 
-const sessionStore = new MySQLStore({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+// const sessionStore = new MySQLStore({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
+
+const sessionStore = new pgSession({
+  conObject: {
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+    // port: db_port,
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${db_port}/${process.env.DB_NAME}`,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 app.use(
@@ -48,9 +65,15 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: sessionStore,
-    cookie: { secure: false }, // Set secure: true if using HTTPS
+    // cookie: { secure: false }, // Set secure: true if using HTTPS
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
   }),
 );
+
+sessionStore.on("error", (err) => {
+  console.error("Session store error:", err);
+});
+
 app.use((req, res, next) => {
   if (!req.session.csrfToken) {
     req.session.csrfToken = generateCSRFTToken();
